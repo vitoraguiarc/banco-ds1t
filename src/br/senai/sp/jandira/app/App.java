bc1d8c4f1d7d69bdb9be5c52c8a88668f01bd164
VITOR AGUIAR CUNHA <21276756@cfp127aluno.local>
2021-09-29 16:51:03 -0300

    Projetinho banco - 29/09

--- src/br/senai/sp/jandira/app/App.java
+++ src/br/senai/sp/jandira/app/App.java
@@ -7,26 +7,23 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Criação da conta da Maria
-		Conta contaMaria = new Conta();
+		Conta contaMaria = new Conta("7845-8");
 		contaMaria.titular = "Maria Antonieta";
-		contaMaria.numero = "7845-8";
 		contaMaria.numeroAgencia = "4214-9";
-		contaMaria.saldo = 500.0;
+		contaMaria.depositar(500);
 		contaMaria.tipo = "Corrente";
 		
 		// Criação da conta do Pedro
-		Conta contaPedro = new Conta();
+		Conta contaPedro = new Conta("6547-6");
 		contaPedro.titular = "Pedro Cabral";
-		contaPedro.numero = "6547-6";
-		contaPedro.saldo = 200.0;
+		contaPedro.depositar(400);
 		contaPedro.tipo = "Poupança";
 		contaPedro.numeroAgencia = "4214-9";
 		
 		// Criação da conta da Ana
-		Conta contaAna = new Conta();
+		Conta contaAna = new Conta("2345-9");
 		contaAna.titular = "Ana Gomes";
-		contaAna.numero = "23145-9";
-		contaAna.saldo = 2000.0;
+		contaAna.depositar(2000);
 		contaAna.tipo = "Corrente";
 		contaAna.numeroAgencia = "4214-9";
 		
@@ -36,7 +33,26 @@ public class App {
 		contaPedro.exibirDetalhes();
 		contaAna.exibirDetalhes();
 		
+		// Depositar 100 reais na conta da Maria
+		contaMaria.depositar(100);
+		contaMaria.exibirDetalhes();
 		
+		// Sacar 300 reais da conta da Maria
+		contaMaria.sacar(300);
+		contaMaria.exibirDetalhes();
+		
+		// Depositar -200 na conta do Pedro
+		contaPedro.depositar(-200);
+		contaPedro.exibirDetalhes();
+		
+		// Sacar -100 da conta da Maria
+		contaMaria.sacar(-200);
+		contaMaria.exibirDetalhes();
+		
+		/* Transferir 100 reais da conta do Pedro para a conta da Maria */
+		contaPedro.transferir(contaMaria, 150);
+		contaPedro.exibirDetalhes();
+		contaMaria.exibirDetalhes();
 
 	}
 
--- src/br/senai/sp/jandira/model/Conta.java
+++ src/br/senai/sp/jandira/model/Conta.java
@@ -2,21 +2,66 @@ package br.senai.sp.jandira.model;
 
 public class Conta {
 	
+	// Atributos da classe Conta
 	public String tipo;
-	public String numero;
+	private String numero;
 	public String numeroAgencia;
 	public String titular;
-	public double saldo;
+	private double saldo;
 	
+	
+	// Construtores da classe Conta
+	public Conta(String numeroConta) {
+		numero = numeroConta;
+	}
+	
+	// Métodos da classe Conta
 	public void depositar(double valorDeposito) {
-		saldo += valorDeposito;
+		
+		System.out.println("");
+		System.out.println("Efetuando depósito...");
+		
+		if (valorDeposito > 0) {
+			saldo += valorDeposito;
+			System.out.println("Depósito efetuado com sucesso :))");
+			System.out.println("Foi depositado " + valorDeposito + " na conta da(o) " + titular);
+		} else {
+			System.out.println("Não foi possível efetuar o depósito :(("
+					+ "\nO valor do deposito deve ser maior que 0");
+		}
 	}
 	
-	public void sacar(double valorSaque) {
-		saldo -= valorSaque;
+	public boolean sacar(double valorSaque) {
+		
+		System.out.println("");
+		System.out.println("Efetuando saque...");
+		
+		if (valorSaque > 0 && valorSaque <= saldo) {
+			saldo -= valorSaque;
+			System.out.println("Saque realido com sucesso :))");
+			System.out.println("Foi feito um saque de " + valorSaque + " da conta da(o) " + titular);
+			return true;
+		} else {
+			System.out.println("Não foi possível sacar :(("
+					+ "\n!! O valor do saque deve ser maior que 0 e dentro do limite da conta !!");
+			return false;
+		} 
 	}
 	
-	public void transferir() {
+	public void transferir(Conta contaDestino, double valorTransferencia) {
+		
+		System.out.println("");
+		System.out.println("Efetuando transfêrencia...");
+		
+		boolean resultado = sacar(valorTransferencia);
+		
+		if (resultado) {
+			contaDestino.depositar(valorTransferencia);
+		} else {
+			System.out.println("Não foi possível realizar a transferência");
+		}
+		
+		
 		
 	}
 	
